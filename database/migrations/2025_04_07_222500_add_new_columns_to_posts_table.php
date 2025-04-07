ccc049f73cdac6b7c73921fa4c2d93acd0991efa
SulaMaxx <sjeewaranga@gmail.com>
2025-04-08 04:01:34 +0530

    add migration

---
+++ database/migrations/2025_04_07_222500_add_new_columns_to_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->timestamp('view_at')->nullable();
+            $table->unsignedBigInteger('view_by')->nullable();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['view_by']);
+            $table->dropColumn(['view_at', 'view_by']);
+        });
+    }
+};
